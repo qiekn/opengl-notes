465ea81b58bc48cac195d2c1ff70fb31afbf52a9
qiekn <qieknxxx@gmail.com>
2025-09-14 22:30:02 +0800

    my first window!
    update readme
    
    download and unzip glfw-3.4 source code
    
    cmake template and hello world program
    
    docs: lower case some sentences and change order
    
    fix: cmake templates
    
    fix: link opengl at macos (you need do this by yourself -.- )
    
    feat: the first window !!!!

---
+++ run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Config
+PROJECT_NAME="main"
+BUILD_DIR="build"
+DEBUGGER="lldb"
+
+# If the build directory does not exist, run cmake first
+if [ ! -d "${BUILD_DIR}" ]; then
+  echo "[INFO] ${BUILD_DIR} not found, mkdir and running cmake..."
+  cmake -S . -B "${BUILD_DIR}" || exit 1
+fi
+
+if [ "$1" = "debug" ]; then
+  ${DEBUGGER} "./${BUILD_DIR}/${PROJECT_NAME}"
+else
+  make -C "${BUILD_DIR}" && make -j"$(nproc)" -C "${BUILD_DIR}" && "./${BUILD_DIR}/${PROJECT_NAME}"
+fi
+
+# vim: ft=sh ts=2 sw=2 et
