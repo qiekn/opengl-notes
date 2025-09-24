0dea2332f31e7487d21467555178a8b3a5cd9e3d
qiekn <qiekndev@gmail.com>
2025-09-24 12:30:07 +0800

    feat: draw multi textures

--- src/the_cherno/src/main.cpp
+++ src/the_cherno/src/main.cpp
@@ -59,10 +59,10 @@ int main() {
   └────────*/
 
   float position[] = {
-      100.0f, 100.0f, 0.0f, 0.0f,  // 0
-      200.0f, 100.0f, 1.0f, 0.0f,  // 1
-      200.0f, 200.0f, 1.0f, 1.0f,  // 2
-      100.0f, 200.0f, 0.0f, 1.0f   // 3
+      -50.0f, -50.0f, 0.0f, 0.0f,  // 0
+      50.0f,  -50.0f, 1.0f, 0.0f,  // 1
+      50.0f,  50.0f,  1.0f, 1.0f,  // 2
+      -50.0f, 50.0f,  0.0f, 1.0f   // 3
   };
 
   unsigned int indices[]{0, 1, 2, 2, 3, 0};
@@ -86,27 +86,6 @@ int main() {
 
   Renderer renderer;
 
-  /*───────┐
-  │ Shader │
-  └────────*/
-
-  glm::mat4 proj = glm::ortho(0.0f, (float)kScreenWidth, 0.0f, (float)kScreenHeight, -1.0f, 1.0f);
-  glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-100, 0, 0));
-  glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(200, 200, 0));
-  glm::mat4 mvp = proj * view * model;  // model-view-projection
-
-  Shader shader("assets/shaders/basic.shader");
-  shader.Bind();
-  shader.SetUniform4f("u_color", 1.0f, 0.5f, 0.2f, 1.0f);
-  shader.SetUniformMat4f("u_mvp", mvp);
-
-  Texture texture("assets/textures/cat.jpg");
-  texture.Bind();
-  shader.SetUniform1i("u_texture", 0);
-
-  float color_b = 1.0f;
-  float increment = -0.05f;
-
   /*────────────┐
   │ ImGUi Setup │
   └─────────────*/
@@ -114,14 +93,34 @@ int main() {
   ImGui::CreateContext();
   ImGuiIO& io = ImGui::GetIO();
   ImGui::StyleColorsDark();
+  ImGuiStyle& style = ImGui::GetStyle();
+  style.WindowRounding = 8.0f;
+  style.FrameRounding = 8.0f;
 
   ImGui_ImplGlfw_InitForOpenGL(window, true);
   ImGui_ImplOpenGL3_Init("#version 330");
 
   // state
-  bool show_demo_window = false;
-  bool show_another_window = true;
-  ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+  glm::vec3 translate_a(200, 200, 0);
+  glm::vec3 translate_b(400, 200, 0);
+
+  /*───────┐
+  │ Shader │
+  └────────*/
+
+  glm::mat4 proj = glm::ortho(0.0f, (float)kScreenWidth, 0.0f, (float)kScreenHeight, -1.0f, 1.0f);
+  glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, 0));
+
+  Shader shader("assets/shaders/basic.shader");
+  shader.Bind();
+  shader.SetUniform4f("u_color", 1.0f, 0.5f, 0.2f, 1.0f);
+
+  Texture texture("assets/textures/cat.jpg");
+  texture.Bind();
+  shader.SetUniform1i("u_texture", 0);
+
+  float color_b = 1.0f;
+  float increment = -0.05f;
 
   /*──────────┐
   │ Main Loop │
@@ -136,36 +135,12 @@ int main() {
     ImGui_ImplGlfw_NewFrame();
     ImGui::NewFrame();
 
-    // 1. Show the big demo window
-    if (show_demo_window) ImGui::ShowDemoWindow(&show_demo_window);
-
-    // 2. Show a simple window that we create ourselves.
     {
-      static float f = 0.0f;
-      static int counter = 0;
+      ImGui::Begin("Positions");
+      ImGui::SliderFloat3("Translation A", &translate_a.x, 0.0f, (float)kScreenWidth);
+      ImGui::SliderFloat3("Translation B", &translate_b.x, 0.0f, (float)kScreenWidth);
 
-      ImGui::Begin("Hello, world!");
-
-      ImGui::Text("This is some useful text.");
-      ImGui::Checkbox("Demo Window", &show_demo_window);
-      ImGui::Checkbox("Another Window", &show_another_window);
-
-      ImGui::SliderFloat("float", &f, 0.0f, 1.0f);             // Edit 1 float using a slider from 0.0f to 1.0f
-      ImGui::ColorEdit3("clear color", (float*)&clear_color);  // Edit 3 floats representing a color
-
-      if (ImGui::Button("Button"))  // Buttons return true when clicked
-        counter++;
-      ImGui::SameLine();
-      ImGui::Text("counter = %d", counter);
       ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
-      ImGui::End();
-    }
-
-    // 3. Show another simple window.
-    if (show_another_window) {
-      ImGui::Begin("Another Window", &show_another_window);
-      ImGui::Text("Hello from another window!");
-      if (ImGui::Button("Close Me")) show_another_window = false;
       ImGui::End();
     }
 
@@ -175,7 +150,18 @@ int main() {
     va.Bind();
     ib.Bind();
 
-    renderer.Draw(va, ib, shader);
+    {
+      glm::mat4 model = glm::translate(glm::mat4(1.0f), translate_a);
+      glm::mat4 mvp = proj * view * model;  // model-view-projection
+      shader.SetUniformMat4f("u_mvp", mvp);
+      renderer.Draw(va, ib, shader);
+    }
+    {
+      glm::mat4 model = glm::translate(glm::mat4(1.0f), translate_b);
+      glm::mat4 mvp = proj * view * model;  // model-view-projection
+      shader.SetUniformMat4f("u_mvp", mvp);
+      renderer.Draw(va, ib, shader);
+    }
 
     // test uniform variable
     if (color_b >= 1.0f)
