4ff37eac500438f46de8cefd142182a3701bd872
qiekn <qieknxxx@gmail.com>
2025-11-04 13:50:08 +0800

    feat: my first triangle

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,40 @@
 // clang-format on
 
 #include <cstdio>
+#include <string>
+
+// Shader code
+std::string vertex_source = R"(
+  #version 330 core
+
+  layout(location = 0) in vec3 aPosition;
+
+  out vec3 vPosition;
+
+  void main() {
+    vPosition = aPosition;
+    gl_Position = vec4(aPosition, 1.0);
+  }
+)";
+
+std::string fragment_source = R"(
+  #version 330 core
+
+  layout(location = 0) out vec4 color;
+
+  in vec3 vPosition;
+
+  void main() {
+    color = vec4(vPosition * 0.5 + 0.5, 1.0);
+  }
+)";
 
 int main(void) {
   // Initialize GLFW library
   if (!glfwInit()) return -1;
 
   // Create a windowed mode window and its OpenGL context
-  GLFWwindow* window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+  GLFWwindow* window = glfwCreateWindow(640, 480, "ck: my first triangle", NULL, NULL);
   if (!window) {
     glfwTerminate();
     return -1;
@@ -29,10 +56,57 @@ int main(void) {
   // Successfully loaded OpenGL
   printf("Loaded OpenGL %d.%d\n", GLAD_VERSION_MAJOR(version), GLAD_VERSION_MINOR(version));
 
+  // clang-format off
+  // Triangle vertices (after MVP transform)
+  float vertices[] = {
+      -0.5f, -0.5f, 0.0f,
+       0.5f, -0.5f, 0.0f,
+       0.0f,  0.5f, 0.0f
+  };
+  // clang-format on
+
+  unsigned int VAO;  // Vertex Array Object
+  glGenVertexArrays(1, &VAO);
+  glBindVertexArray(VAO);
+
+  unsigned int VBO;  // Vertex Buffer Object
+  glGenBuffers(1, &VBO);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+  glEnableVertexAttribArray(0);
+
+  glBindVertexArray(0);  // Unbind VAO after configuring all the vertex attribute pointers and buffers,
+                         // Unbinding the VAO prevents accidental modification by subsequent OpenGL calls.
+
+  // Compile shaders
+  // 1. Vertex shader
+  unsigned int vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+  auto source = (const GLchar*)vertex_source.c_str();
+  glShaderSource(vertex_shader, 1, &source, NULL);
+  glCompileShader(vertex_shader);
+  // 2. Fragment shader
+  unsigned int fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+  source = (const GLchar*)fragment_source.c_str();
+  glShaderSource(fragment_shader, 1, &source, NULL);
+  glCompileShader(fragment_shader);
+
+  // Shader Program
+  unsigned int shader_program = glCreateProgram();
+  glAttachShader(shader_program, vertex_shader);
+  glAttachShader(shader_program, fragment_shader);
+  glLinkProgram(shader_program);
+  glDeleteShader(vertex_shader);
+  glDeleteShader(fragment_shader);
+
   // Loop until the user closes the window
   while (!glfwWindowShouldClose(window)) {
     // Render here
     glClear(GL_COLOR_BUFFER_BIT);
+
+    glUseProgram(shader_program);
+    glBindVertexArray(VAO);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
 
     // Swap front and back buffers
     glfwSwapBuffers(window);
@@ -40,6 +114,11 @@ int main(void) {
     // Poll for and process events
     glfwPollEvents();
   }
+
+  // Cleanup OpenGL resources
+  glDeleteProgram(shader_program);
+  glDeleteVertexArrays(1, &VAO);
+  glDeleteBuffers(1, &VBO);
 
   glfwTerminate();
   return 0;
