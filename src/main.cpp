ac01c904b33c2876d9c1352a9603e3cf39a573e7
qiekn <qieknxxx@gmail.com>
2025-11-04 12:50:16 +0800

    feat: GLAD loader

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,43 @@
-#include <GLFW/glfw3.h>
+// clang-format off
+#include <glad/gl.h>
+#include <GLFW/glfw3.h> // GLFW (include after glad)
+// clang-format on
 
-/**
- * @brief glfw example codee
- * @link: https://www.glfw.org/documentation.html
- */
+#include <cstdio>
+
 int main(void) {
-  GLFWwindow* window;
-
-  /* Initialize the library */
+  // Initialize GLFW library
   if (!glfwInit()) return -1;
 
-  /* Create a windowed mode window and its OpenGL context */
-  window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+  // Create a windowed mode window and its OpenGL context
+  GLFWwindow* window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
   if (!window) {
     glfwTerminate();
     return -1;
   }
 
-  /* Make the window's context current */
+  // Make the window's context current
   glfwMakeContextCurrent(window);
 
-  /* Loop until the user closes the window */
+  // GLAD
+  int version = gladLoadGL(glfwGetProcAddress);
+  if (version == 0) {
+    printf("Failed to initialize OpenGL context\n");
+    return -1;
+  }
+
+  // Successfully loaded OpenGL
+  printf("Loaded OpenGL %d.%d\n", GLAD_VERSION_MAJOR(version), GLAD_VERSION_MINOR(version));
+
+  // Loop until the user closes the window
   while (!glfwWindowShouldClose(window)) {
-    /* Render here */
+    // Render here
     glClear(GL_COLOR_BUFFER_BIT);
 
-    /* Swap front and back buffers */
+    // Swap front and back buffers
     glfwSwapBuffers(window);
 
-    /* Poll for and process events */
+    // Poll for and process events
     glfwPollEvents();
   }
 
